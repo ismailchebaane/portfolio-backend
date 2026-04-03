c1cc3b52ebd477d1df901db4d20840fb46662295
ismail chebaane <105134011+ismailchebaane@users.noreply.github.com>
2026-04-03 15:39:54 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,10 +8,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
